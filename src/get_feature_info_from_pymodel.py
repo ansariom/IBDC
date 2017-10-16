c984c5a89f3fb72201457d148ca2db0591383968
Mitra Ansariola <mitra.ansari@gmail.com>
2017-10-16 12:45:13 -0700

    feature_info_2

--- src/get_feature_info_from_pymodel.py
+++ src/get_feature_info_from_pymodel.py
@@ -10,17 +10,27 @@ import numpy as np
 import pandas as pd
 import csv
 from sklearn import preprocessing
+from dask.array.core import asarray
+from sklearn import metrics
+from sklearn.metrics import average_precision_score
+from numpy import int32
+
 
 
 '================================================'
 '   run saved model on given set of features  '
 '================================================'
-def run_model(x_test, x_test_tss_id, outfile, model_file_name):
-    trained_model = pickle.load(open(model_file_name, 'r'))
+def run_model(x_test, x_test_tss_id, outfile, model_file_name, class_labels = None):
+    trained_model = pickle.load(open(model_file_name, 'rb'))
     result = trained_model.predict_proba(x_test)
-    res_records = np.rec.fromarrays((x_test_tss_id, result), names= ('feature', 'prob0', 'prob1'))
+    res_records = np.rec.fromarrays((x_test_tss_id, result[:,0].ravel(), result[:,1].ravel()), names= ('tss_name', 'prob0', 'prob1'))
     df = pd.DataFrame.from_records(res_records)
-    df.to_csv(outfile, sep = "\t", columns = ('feature', 'prob1'), index = False, header = False)
+    df.to_csv(outfile, sep = "\t", columns = ('tss_name', 'prob0', 'prob1'), index = False, header = True)
+    
+    fpr, tpr, thresholds = metrics.roc_curve(class_labels, result[:,1], pos_label=1)
+    auroc = metrics.auc(fpr, tpr)
+    average_precision = average_precision_score(class_labels, result[:,1])
+    print(auroc, average_precision)
 
 if __name__ == '__main__':
     args = sys.argv[1:]
@@ -32,25 +42,26 @@ if __name__ == '__main__':
     # read input features, 1st column is tss_id and last column is class label
     x_test = []
     first_line = True
-    with open(all_features_csv, 'rt') as csvfile:
+    with open(all_features_csv, 'rb') as csvfile:
         spamreader = csv.reader(csvfile, delimiter=',')
         for row in spamreader:
             if (first_line):
                 features_header = np.asarray(row[0:-1])
                 first_line = False
-                continue
+                continue    
             x_test.append(np.asarray(row))
     x_test = np.asanyarray(x_test)
 
     # 1- compute all_features_scaled using saved train_set 
     x_test_features = np.asanyarray(x_test[:,1:-1], np.float32)
     print(x_test_features.shape)
-    x_test_tss_ids = x_test[:,0]
+    x_test_tss_ids = asarray(x_test[:,0])
+    print(x_test_tss_ids.shape)
     x_train = np.load(train_set)
     scaler = preprocessing.StandardScaler(with_mean=True, with_std=True).fit(x_train)
     x_test_scaled = scaler.transform(x_test_features)
     
-    run_model(x_test_scaled, x_test_tss_ids, outfile, model_file)
+    run_model(x_test_scaled, x_test_tss_ids, outfile, model_file, np.asanyarray(x_test[:,-1], int32))
     
     
     
