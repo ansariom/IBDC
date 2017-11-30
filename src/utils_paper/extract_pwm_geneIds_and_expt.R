bb02a84b8c89264f8deb1d292fade59eee6cb9e0
Mitra Ansariola <ansariom@waterman.cgrb.oregonstate.local>
2017-11-30 14:41:39 -0800

    pwm_gene_expr

--- src/utils_paper/extract_pwm_geneIds_and_expt.R
+++ src/utils_paper/extract_pwm_geneIds_and_expt.R
@@ -1,26 +1,47 @@
-huges_genes <- read.delim("~/Downloads/huges_genes_mats.txt", fill = T, col.names = c("pwm", "gene_id"), sep = "\t")
-huges_genes <- extract(huges_genes, pwm, c("matrix_id", "suffix"), regex = "(M\\d+)_(.*)", remove = F)
-transcfac_genes <- read.delim("~/Downloads/ath.mats_genes.txt", fill = T, sep = "\t", col.names = c("pwm", "gene_id"))
-coef_table <- read.table("~/Downloads/roe_only_coef_table.txt", col.names = c("feature", "coef"))
-diff <- read.table("~/Downloads/diff_exp_results.txt", header = T)
+#!/usr/bin/Rscript
 
+library(tidyr)
+huges_file <- "huges_genes_mats_multi_rows.txt"
+trans_file <- "transfac_genes_mats_multi_rows.txt"
+coef_file <- "roe_only_coef_table.txt"
+diff_file <- "diff_exp_results_with_geneIds.txt"
 
-huges_genes <- na.omit(huges_genes)
+huges_genes <- read.table(huges_file, header = F, col.names=c("mat_id", "gene_id"))
+transcfac_genes <- read.delim(trans_file, sep = "\t", col.names = c("mat_id", "gene_id"))
+diff <- read.table(diff_file, header = T)
+coef_table <- read.table(coef_file, col.names = c("feature", "coef"))
+
+mat_table <- rbind(transcfac_genes, huges_genes)
+diff <- diff[, colnames(diff) %in% c("gene_id", "transcript_no", "Accession", "qval", "pval", "b")]
+diff <- na.omit(diff)
+
+mat_diff <- merge(mat_table, diff, by = "gene_id")
+avg_expr <- aggregate(mat_diff[, colnames(mat_diff) %in% c("pval", "qval", "b")], list(gene_id = mat_diff$gene_id), mean)
+max_expr <- aggregate(mat_diff[, colnames(mat_diff) %in% c("pval", "qval", "b")], list(gene_id = mat_diff$gene_id), max)
+min_expr <- aggregate(mat_diff[, colnames(mat_diff) %in% c("pval", "qval", "b")], list(gene_id = mat_diff$gene_id), min)
+
+avg_max_exp  <- merge(avg_expr, max_expr, by = "gene_id", suffixes=c("_avg", "_max"))
+avg_min_max_exp <- merge(avg_max_exp, min_expr, by = "gene_id")
+colnames(avg_min_max_exp)[which(colnames(avg_min_max_exp) == "b")] <- "b_min"
+colnames(avg_min_max_exp)[which(colnames(avg_min_max_exp) == "pval")] <- "pval_min"
+colnames(avg_min_max_exp)[which(colnames(avg_min_max_exp) == "qval")] <- "qval_min"
+
 
 coef_table <- extract(coef_table, feature, c("pwm", "strand", "window"), regex = "(.+?)_(FWD|REV)_(\\d+)", remove = F)
-coef_table <- extract(coef_table, pwm, c("matrix_id", "suffix"), regex = "(M\\d+)_(.*)", remove = F)
-c1 <- merge(coef_table, transcfac_genes, by.x = "matrix_id", by.y = "pwm", all.x = T)
+coef_table <- extract(coef_table, pwm, c("mat_id", "suffix"), regex = "(M\\d+)_(.*)", remove = F)
+coef_table$mat_id <- ifelse(coef_table$suffix == "1.02", paste(coef_table$mat_id, "_", "1.02", sep = ""), coef_table$mat_id)
 
-c1 <- merge(c1, huges_genes, by = "matrix_id", all.x = T)
 
-c1$pwm.y <- NULL
-c1$strand <- NULL
-c1$suffix.x <- NULL
-c1$suffix.y <- NULL
-c1$matrix_id <- NULL
+coef_genes <- merge(coef_table, mat_table, by = "mat_id", all.x = T)
+final_out <- merge(coef_genes, avg_min_max_exp, by = "gene_id", all.x = T)
 
-c1$gene_ids <- paste(c1$gene_id.x, c1$gene_id.y, sep = "")
-c1$gene_id.x <- NULL
-c1$gene_id.y <- NULL
-c1 <- c1[order(-abs(c1$coef)),]
+sel_cols <- c("feature", "coef", "gene_id","b_avg", "b_min", "b_max" , "pval_avg", "pval_min" ,"pval_max", "qval_avg", "qval_min", "qval_max")
+final_out <- final_out[, sel_cols]
+final_out <- final_out[order(-abs(final_out$coef)),]
+final_out$tissue <- ifelse(final_out$b_avg > 2, "root", ifelse(final_out$b_avg < -2, "leaf", "None"))
+final_out$coef_indication_tissue <- ifelse(final_out$coef < 0, "root", "leaf")
+final_out$feature_type <- "TFBS"
+final_out$feature_type[grepl("(OC_P_ROOT)|(OC_P_LEAF)", final_out$feature)] <- "OC"
 
+
+write.csv(final_out, "model_weight_coefficient_table_with_gene_expr.csv", quote = F, sep = ",", row.names = F)
